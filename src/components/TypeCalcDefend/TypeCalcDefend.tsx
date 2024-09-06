e0cb9e78813701d5a62f2154c59fc996efd9184c
hanbinchoi <wkghskak@naver.com>
2024-09-06 12:40:40 +0900

    Style: 방어 타입 계산 세로 갭 수정

--- src/components/TypeCalcDefend/TypeCalcDefend.tsx
+++ src/components/TypeCalcDefend/TypeCalcDefend.tsx
@@ -6,7 +6,7 @@ import { TypeCalcDefendResult } from '../TypeCalcDefendResult/TypeCalcDefendResu
 export const TypeCalcDefend = () => {
   return (
     <div className="grid grid-cols-2 gap-16">
-      <div className="flex flex-col px-2 gap-6">
+      <div className="flex flex-col px-2 gap-8">
         <TypeCalcOption index={0} />
         <TypeCalcOption index={1} />
         <CommonSelect usage="defenceAbility" />
