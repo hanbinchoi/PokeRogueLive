5cc2226eff13121aff89b136a5c5f0cc01bd55b6
hanbinchoi <wkghskak@naver.com>
2024-09-06 12:39:28 +0900

    Feat: 방어 계산 결과에 개수 추가

--- src/components/TypeCalcDefendResult/TypeCalcDefendResult.tsx
+++ src/components/TypeCalcDefendResult/TypeCalcDefendResult.tsx
@@ -29,7 +29,9 @@ export const TypeCalcDefendResult = () => {
       <div className="flex flex-col gap-8 py-8">
         {Array.from(defendResult.entries()).map(([key, types]) => (
           <div key={key} className="flex flex-col gap-2">
-            <div className="text-[16px] font-semibold">{key}x 피해</div>
+            <div className="text-[16px] font-semibold">
+              {key}x 피해 ({types.length})
+            </div>
             <div className="flex gap-2 flex-wrap">
               {types.map((type) => (
                 <TypeBadge key={type} type={type} size="medium" />
