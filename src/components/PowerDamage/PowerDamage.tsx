5fbbf1ca559342b522926feb2d735e3ab7af3597
hanbinchoi <wkghskak@naver.com>
2024-12-19 05:56:21 +0900

    Feat: 데미지 범위 별 색상 다르게 적용

--- src/components/PowerDamage/PowerDamage.tsx
+++ src/components/PowerDamage/PowerDamage.tsx
@@ -39,7 +39,6 @@ export const PowerDamage = ({ moveUrl }: PowerDamageProps) => {
       MoveDetail.damage_class.name === 'status'
     )
       return;
-
     const { newDamages, damageContext: newDamageContext } = calcPower({
       attackPokemon,
       defendPokemon,
@@ -58,6 +57,16 @@ export const PowerDamage = ({ moveUrl }: PowerDamageProps) => {
 
   if (isError) return <div>데이터 오류 발생</div>;
 
+  if (
+    !attackPokemonStats ||
+    !defendPokemonStats ||
+    !attackPokemon ||
+    !defendPokemon ||
+    !MoveDetail ||
+    MoveDetail.damage_class.name === 'status'
+  )
+    return;
+
   if (MoveDetail?.damage_class.name === 'status')
     return (
       <div className="py-9 flex items-center justify-center flex-col h-full">
@@ -68,20 +77,36 @@ export const PowerDamage = ({ moveUrl }: PowerDamageProps) => {
     );
 
   return (
-    <div className="py-9 flex items-center flex-col gap-2">
-      <div className="text-xl mb-2 flex gap-1">
+    <div className="py-9 flex items-center flex-col gap-3">
+      <div className="text-xl flex gap-1">
         데미지
         <Tooltip text={'해당 기술로 10회 타격 시 데미지 계산 결과입니다.'} />
       </div>
-      <div className="text-2xl font-bold">{damages.join(', ')}</div>
+      <div className="text-2xl font-bold flex gap-2">
+        {damages.map((damage, i) => {
+          if (damage / defendPokemonStats.hp > 0.66)
+            return (
+              <p key={i} className="text-red-30">
+                {damage}
+              </p>
+            );
+          if (damage / defendPokemonStats.hp > 0.33)
+            return <p key={i}>{damage}</p>;
+          return (
+            <p key={i} className="text-blue-30">
+              {damage}
+            </p>
+          );
+        })}
+      </div>
       <div className="text-center">
         {damageContext &&
           Object.entries(damageContext)
-            .filter(([_, value]) => value !== null) // null이 아닌 값만 필터링
+            .filter(([_, value]) => value !== null)
             .map(([key, value]) => (
               <div key={key} className="text-sm text-gray-50">
                 {value}
-              </div> // 각 메시지를 div로 감싸서 출력
+              </div>
             ))}
       </div>
     </div>
