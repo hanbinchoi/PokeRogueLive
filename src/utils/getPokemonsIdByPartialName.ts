8fe98844fb4ff19887e14c86a308badbcede84cd
hanbinchoi <wkghskak@naver.com>
2025-01-06 08:42:45 +0900

    Comment: getPokemonsIdByPartialName 함수 주석 작성

--- src/utils/getPokemonsIdByPartialName.ts
+++ src/utils/getPokemonsIdByPartialName.ts
@@ -1,5 +1,11 @@
 import { POKEMON_LIST_IN_KOREAN } from '@/constants/contents';
 
+/**
+ * 입력된 문자열이 이름에 포함되는 포켓몬의 ID 목록을 반환하는 함수.
+ *
+ * @param input - 입력 문자열 (`string`)
+ * @return 해당하는 포켓몬 Id 목록 (`number[]`)
+ */
 export default function getPokemonsByPartialName(input: string): number[] {
   return POKEMON_LIST_IN_KOREAN.map((word, index) =>
     word.includes(input) ? index + 1 : -1,
