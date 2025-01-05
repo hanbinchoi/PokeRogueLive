7c5bd09b286f7ccfc31668ac141f82adbc8a577a
hanbinchoi <wkghskak@naver.com>
2025-01-06 08:36:52 +0900

    Comment: ability 한글화 함수 주석 작성

--- src/utils/getAbilityDescInKorean.ts
+++ src/utils/getAbilityDescInKorean.ts
@@ -1,5 +1,12 @@
 import { FlavorTextEntryDataProps } from '@/types/data';
 
+/** API로 부터 받은 특성 설명 데이터 중 한글 데이터를 추출하는 함수
+ *
+ * 한글 데이터를 미지원하는 경우 영문을 반환
+ *
+ * @param moves - API로 부터 받은 이름 목록 (`FlavorTextEntryDataProps[]`)
+ * @return 추출된 이름 (`string`)
+ */
 export default function getAbilityDescInKorean(
   flavors: FlavorTextEntryDataProps[],
 ) {
