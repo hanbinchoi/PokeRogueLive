22fed01e26666c6e8b07924f8a8eaf331db6b902
hanbinchoi <wkghskak@naver.com>
2025-01-08 16:59:05 +0900

    Comment: CommonCheckbox 주석 추가

--- src/components/common/CommonCheckBox.tsx
+++ src/components/common/CommonCheckBox.tsx
@@ -4,11 +4,25 @@ interface CommonCheckBoxProps {
   setIsChecked: (checked: boolean) => void;
 }
 
+/**
+ * 공통 체크박스 컴포넌트
+ *
+ * - 사용자가 체크박스를 클릭하여 선택/해제 상태를 관리할 수 있습니다.
+ * - 외부에서 `isChecked`와 `setIsChecked`로 상태를 제어합니다.
+ *
+ * @param label 체크박스 텍스트 (`string`)
+ * @param isChecked 체크박스 초기 선택 상태 (`boolean`)
+ * @param setIsChecked 체크박스 상태를 업데이트하는 함수
+ */
 export const CommonCheckBox = ({
   label,
   isChecked,
   setIsChecked,
 }: CommonCheckBoxProps) => {
+  /**
+   * 체크박스 상태 변경 핸들러
+   * - 사용자가 체크박스를 선택/해제했을 때 호출됩니다.
+   */
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(event.target.checked);
   };
@@ -24,7 +38,6 @@ export const CommonCheckBox = ({
         checked={isChecked}
         onChange={handleCheckboxChange}
         aria-checked={isChecked}
-        tabIndex={3}
       />
       {label}
     </label>
