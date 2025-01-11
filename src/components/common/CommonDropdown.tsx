39fd03d73e3de77dc8e98142e1542faa05c4248a
hanbinchoi <wkghskak@naver.com>
2025-01-11 15:10:28 +0900

    Test: PokemonSearchForm unit test

--- src/components/common/CommonDropdown.tsx
+++ src/components/common/CommonDropdown.tsx
@@ -60,7 +60,7 @@ export const CommonDropdown = ({
     <div
       ref={dropdownRef}
       className={twJoin(
-        'absolute z-10 w-full min-w-[120px] max-w-[162px] text-sm lg:text-[14px] bg-white-100 border-2 border-t rounded shadow max-h-20 sm:max-h-40 overflow-y-auto ',
+        'dropdown-container absolute z-10 w-full min-w-[120px] max-w-[162px] text-sm lg:text-[14px] bg-white-100 border-2 border-t rounded shadow max-h-20 sm:max-h-40 overflow-y-auto ',
         className,
       )}>
       {filteredOptions.length === 0 && (
