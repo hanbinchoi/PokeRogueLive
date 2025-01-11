acb2f68b4a41ecf392ef0f0c7cc037fa37980285
hanbinchoi <wkghskak@naver.com>
2025-01-11 19:36:56 +0900

    Test: AttackOptionButton unit test

---
+++ src/components/type-calculator/AttackOptionButton.cy.tsx
@@ -0,0 +1,36 @@
+import { AttackOptionButton } from './AttackOptionButton';
+import { AttackOptionUsage, SpecialAttackAbility } from '@/constants/contents';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+describe('AttackOptionButton', () => {
+  const options = [
+    SpecialAttackAbility.GLASSES,
+    SpecialAttackAbility.PERSEVERANCE,
+  ];
+
+  const createQueryClient = () => new QueryClient();
+  beforeEach(() => {
+    cy.mount(
+      <QueryClientProvider client={createQueryClient()}>
+        <AttackOptionButton
+          title="Attack Type"
+          options={options}
+          usage={AttackOptionUsage.MOVE}
+        />
+      </QueryClientProvider>,
+    );
+  });
+
+  it('제공된 옵션과 제목이 화면에 렌더링 됩니다.', () => {
+    cy.get('p').should('contain', 'Attack Type'); // 제목 확인
+    cy.get('input[type="radio"]').should('have.length', options.length); // 라디오 버튼 수 확인
+    cy.get('label').each((label, index) => {
+      cy.wrap(label).should('contain', options[index]); // 각 옵션이 제대로 렌더링되는지 확인
+    });
+  });
+
+  it('버튼 체크 시 화면에 반영됩니다.', () => {
+    cy.get('input[type="radio"]').first().check();
+    cy.get('input[type="radio"]').first().should('be.checked');
+  });
+});
