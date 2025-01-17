07f0fa2a274081496b8d3891f57b90e011a0fdbf
hanbinchoi <wkghskak@naver.com>
2025-01-18 03:55:32 +0900

    Fix: TypeBadge 크기 수정

--- src/components/common/TypeBadge.tsx
+++ src/components/common/TypeBadge.tsx
@@ -54,7 +54,7 @@ export const TypeBadge = ({ type, size = 'medium' }: TypeBadgeProps) => {
 };
 
 const TYPE_BADGE_SIZE: TypeBadgeSizeProps = {
-  small: 'text-xs font-bold px-2 lg:px-3 py-1 rounded-lg',
+  small: 'text-xs font-bold px-2 lg:px-2 py-1 rounded-lg',
   medium:
-    'text-sm sm:text-md md:text-base px-2 md:px-3 lg:px-4 py-1 rounded-lg font-bold w-fit',
+    'text-sm sm:text-md md:text-base px-2 md:px-3 lg:px-3 py-1 rounded-lg font-bold w-fit',
 };
